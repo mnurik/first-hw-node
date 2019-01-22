12972c5b5987b0bd4a7c78d459b8a65710576912
Nurlan <nurlanmirzay@gmail.com>
2019-01-23 00:15:33 +0100

    hw3 in progress

--- utils/streams.js
+++ utils/streams.js
@@ -1,33 +1,20 @@
 #!/usr/bin/env node
+const fs = require('fs');
 const program = require('commander');
+const config = require('./../config/config.json');
 
-const actionHandler = (name, val) => {
-  console.log('====================================');
-  console.log(name);
-  console.log('====================================');
-  functions[name](val)
-};
+const actionHandler = (name, value) => functions[name](value);
 
 const functions = {
-  reverse: (str) => console.log(str.split('').reverse().join(''))
-} 
+  reverse: (str) => console.log(str.split('').reverse().join('')),
+  transform: (str) => console.log(str.split('').join('-')),
+  outputFile: (fileName) => fs.readFile(`./${config.path}${fileName}`, 'utf8', (err, data) => {
+    if (err) console.log(err);
+    console.log(data);
+  })
+}
 
 program
-  .version('0.0.1')
-  .command('-a, --action = <str>', 'A function', actionHandler)
-  // .option('-f, --float <n>', 'A float argument', parseFloat)
-  // .option('-r, --range <a>..<b>', 'A range', range)
-  // .option('-l, --list <items>', 'A list', list)
-  // .option('-o, --optional [value]', 'An optional value')
-  // .option('-c, --collect [value]', 'A repeatable value', collect, [])
-  // .option('-v, --verbose', 'A value that can be increased', increaseVerbosity, 0)
+  .version('0.1.0')
+  .option('-a, --action <name> [value]', 'A function', actionHandler, 'test.csv')
   .parse(process.argv);
-
-// console.log(' float: %j', program.float);
-// console.log(' optional: %j', program.optional);
-// program.range = program.range || [];
-// console.log(' range: %j..%j', program.range[0], program.range[1]);
-// console.log(' list: %j', program.list);
-// console.log(' collect: %j', program.collect);
-// console.log(' verbosity: %j', program.verbose);
-// console.log(' args: %j', program.args);
