74b27a186b1448d8e1385b1844545a66479a619a
Nurlan Mirzayev <nurlanmirzay@gmail.com>
2019-01-29 17:23:32 +0100

    Review changes

--- utils/streams.js
+++ utils/streams.js
@@ -10,8 +10,6 @@ const functions = {
   outputFile: (fileName) => fs.createReadStream(`./${config.path}${fileName}`)
 }
 
-let fileFunction = () => {};
-
 const defineAction = name => {
   if (['reverse', 'transform'].includes(name)) {
     repl.start({
@@ -20,7 +18,7 @@ const defineAction = name => {
     });
   }
   else {
-    fileFunction = functions[name];
+    fileFunction = functions[name]; //outputFile
   }
 }
 
@@ -32,8 +30,28 @@ const defineFile = fileName => {
   });
 }
 
+const actionsFactory = registry => name => {
+  if (typeof registry[name] !== 'function') {
+    throw ReferenceError(`Unknown action "${name}"`)
+  }
+
+  return registry[name]
+}
+
+const getActionByName = actionsFactory(functions);
+
+// 1. check if file exists
+// 2. check if file is readable
+// 3. create R stream
+// 4. return R stream
+openFile = fileName => {
+  return // fileStream
+}
+
 program
   .version('0.1.0')
-  .option('-a, --action <name>', 'A function', defineAction)
-  .option('-f, --file <name>', 'A file name', defineFile)
+  .option('-a, --action <name> [someText]', 'A function', getActionByName2, "eval")
+  .option('-f, --file <fileName>', 'A file name', openFile)
   .parse(process.argv);
+
+console.log(program.args);
